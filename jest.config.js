528f5643a6643c2a490d9ee86b2299768d73f4d5
Anuj Rana <anujaryatitanic300@gmail.com>
2019-05-25 12:23:24 +0530

    test setup using enzyme and jest

---
+++ jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "roots": [
+        "./src"
+    ],
+    "transform": {
+        "^.+\\.tsx?$": "ts-jest"
+    },
+    "testRegex": "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+    "moduleFileExtensions": [
+        "ts",
+        "tsx",
+        "js",
+        "jsx",
+        "json",
+        "node"
+    ],
+    "testEnvironment": "node",
+    "snapshotSerializers": ["enzyme-to-json/serializer"],
+    "setupTestFrameworkScriptFile": "./src/setupEnzyme.ts"
+}
